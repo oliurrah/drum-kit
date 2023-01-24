8dc279badcf7c36ebe894691670239cebd80ca43
Oliur Rahman <oliurrah.n1@gmail.com>
2023-01-24 22:12:05 +0600

    first-commit

---
+++ index.js
@@ -0,0 +1,63 @@
+for(var i=0;i<document.querySelectorAll(".drum").length; i++)
+{
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    var key= this.innerHTML;
+    MakeSound(key);
+    ButtonAnimation(key);
+
+
+  });
+
+}
+document.addEventListener("keydown", function(event){
+  MakeSound(event.key);
+  ButtonAnimation(event.key);
+
+});
+
+
+function MakeSound(key){
+  switch (key) {
+    case "w":
+    var audio= new Audio("sounds/tom-1.mp3");
+    audio.play();
+      break;
+    case "a":
+    var audio= new Audio("sounds/tom-2.mp3");
+    audio.play();
+        break;
+    case "s":
+    var audio= new Audio("sounds/tom-3.mp3");
+    audio.play();
+          break;
+    case "d":
+    var audio= new Audio("sounds/tom-4.mp3");
+    audio.play();
+          break;
+    case "j":
+    var audio= new Audio("sounds/crash.mp3");
+    audio.play();
+          break;
+    case "k":
+    var audio= new Audio("sounds/kick-bass.mp3");
+    audio.play();
+          break;
+    case "l":
+    var audio= new Audio("sounds/snare.mp3");
+    audio.play();
+          break;
+    default:
+
+  }
+
+}
+
+function ButtonAnimation(key){
+  var Button= document.querySelector("."+key);
+  Button.classList.add("pressed");
+  setTimeout(function(){
+    Button.classList.remove("pressed");
+  },100);
+}
+//var audio= new Audio("sounds/tom-1.mp3");
+//audio.play();
